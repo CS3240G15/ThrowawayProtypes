4d54699e5fe646da8d49f766ec66ff3ef5884eba
Nick <grimchains@gmail.com>
2013-03-18 09:36:23 -0400

    Added more cases
    Still need clarification on cases 0100 and 1000

--- OnboardTest.java
+++ OnboardTest.java
@@ -27,8 +27,25 @@ public class OnboardTest
 		    		//fromControl = dis.readUTF();
 		    		//fromControl.split();
 		    		switch(fromControl[0]){
+						case "0000": // move in a direction (We'll assume motor A is left and motor B is right)
+							if (fromControl[1].equals("left")) {
+								b.getMotor("A").reverse(Integer.parseInt(fromControl[2]));
+								b.getMotor("B").forward(Integer.parseInt(fromControl[2]));
+							}
+							else if (fromControl[1].equals("right") {
+								b.getMotor("A").forward(Integer.parseInt(fromControl[2]));
+								b.getMotor("B").reverse(Integer.parseInt(fromControl[2]));
+							}
+							else if (fromControl[1].equals("forward") {
+								b.getMotor("A").forward(Integer.parseInt(fromControl[2]));
+								b.getMotor("B").forward(Integer.parseInt(fromControl[2]));
+							}
+							else if (fromControl[1].equals("reverse") {
+								b.getMotor("A").reverse(Integer.parseInt(fromControl[2]));
+								b.getMotor("B").reverse(Integer.parseInt(fromControl[2]));
+							}
 		    			case "0001": // move individual motor
-							if (fromControl[2].equals("1")) {
+							if (fromControl[2].equals("forward")) {
 								b.getMotor(fromControl[3]).forward(Integer.parseInt(fromControl[1]));
 							}
 							else {
@@ -37,10 +54,20 @@ public class OnboardTest
 		    				send("Motor " + fromControl[3] + " is moving " + fromControl[2] + " at speed " + fromControl[1]);
 		    				//System.exit(0);
 		    				break;
+						case "0010": // stop movement
+							for (int i = 0; i < b.m.size(); i++) {
+								b.m.stop();
+							}
 		    			case "0011": // sensor reading
 							send("Sensor " + fromControl[1] + ": " + b.getSensor(fromControl[1]).readValue());
 		    				//System.exit(0);
 		    				break;
+						case "0100": //Needs some clarification
+						case "0101": // emergency stop
+							emergencyStop();
+						case "0110": // abort
+							System.exit(1);
+						case "1000": //Needs some clarification
 		    			case "exit":
 		    				System.exit(0);
 		    				break;
